1da1fea06d73cd47223500fffed1e720783ff334
shashank srivastava <shashank.1696@gmail.com>
2020-07-16 19:05:26 +0530

    FlipCoinSimulatorWhoWin

--- FlipCoinSimulator.sh
+++ FlipCoinSimulator.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
-result=$((RANDOM%2))
-echo $result
-
+read -p "enter no of times you want to play : " number
+headWon=0
+tailWon=0
+count=0
+while [ $count -lt $number ]
+do
+	result=$((RANDOM%2))
+	if [[ $result -eq 1 ]]
+	then
+		((headWon++))
+	else
+		((tailWon++))
+	fi
+	((count++))
+done
+echo "head won  $headWon times"
+echo "tail won  $tailWon times"
 
